e1c824125039a743cf955fa3fcde94994e1d5894
Brig Lamoreaux <bklamoreaux@gmail.com>
2021-04-06 19:11:58 -0700

    Move code to script

---
+++ script/template.py
@@ -0,0 +1,35 @@
+"""Json to Json Conversion using jinja templates."""
+import json
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+def load_data(filename):
+    """Load source file."""
+    source_path = os.path.join("tests", "jinja_template", filename)
+    with open(source_path, "r") as myfile:
+        text = myfile.read()
+    data = json.loads(text)
+    return data
+
+
+def save_data(data):
+    """Save data to json file."""
+    save_path = os.path.join("tests", "jinja_template", "rendered.json")
+    with open(save_path, "w") as json_file:
+        json.dump(data, json_file, indent=2, separators=(",", ": "))
+
+
+def convert_json(data, template_path, template_name="transform.json"):
+    """Convert Json data for a given template."""
+    # Load Environment
+    loader = FileSystemLoader(template_path)
+    env = Environment(loader=loader)
+
+    # Get Template
+    template = env.get_template(template_name)
+
+    # Transform
+    result_string = template.render(data)
+
+    return json.loads(result_string)
--- tests/jinja_template/test_jinja_template.py
+++ tests/jinja_template/test_jinja_template.py
@@ -1,40 +1,12 @@
 """Test jinja templates."""
-import json
-import os
-
-from jinja2 import Environment, FileSystemLoader
-
-
-def load_data(filename):
-    """Load source file."""
-    source_path = os.path.join("tests", "jinja_template", filename)
-    with open(source_path, "r") as myfile:
-        text = myfile.read()
-    data = json.loads(text)
-    return data
-
-
-def save_data(data):
-    """Save data to json file."""
-    save_path = os.path.join("tests", "jinja_template", "rendered.json")
-    with open(save_path, "w") as json_file:
-        json.dump(data, json_file, indent=2, separators=(",", ": "))
+from script import template
 
 
 def test_json_nested_data():
     """Test Transforming source to target"""
+    template_path = "tests/jinja_template"
+    template_name = "transform.json"
+    data = template.load_data("source.json")
+    message = template.convert_json(data, template_path, template_name)
 
-    loader = FileSystemLoader("tests/jinja_template")
-    env = Environment(loader=loader)
-
-    filename = "transform.json"
-    template = env.get_template(filename)
-
-    data = load_data("source.json")
-    message = template.render(data)
-
-    save_data(message)
-    # save_data(json.loads(message))
-    assert True
-
-test_json_nested_data()
+    assert type(message) == dict
